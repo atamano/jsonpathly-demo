2b81aba507c0faffabbe3fa7ddf37910d8c9a704
atamano <antoine.tamano@gmail.com>
2022-04-23 23:31:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo page of the jsonpathly project
+
+[Demo is available here](https://atamano.github.io/jsonpathly-demo/)
---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { Container, Typography } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import 'ace-builds/src-noconflict/ace';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-monokai';
+import { query } from 'jsonpathly';
+import * as React from 'react';
+import AceEditor from 'react-ace';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const DEFAULT_INPUT = {
+  store: {
+    book: [
+      {
+        category: 'reference',
+        author: 'Nigel Rees',
+        title: 'Sayings of the Century',
+        price: 8.95,
+      },
+      {
+        category: 'fiction',
+        author: 'Evelyn Waugh',
+        title: 'Sword of Honour',
+        price: 12.99,
+      },
+      {
+        category: 'fiction',
+        author: 'Herman Melville',
+        title: 'Moby Dick',
+        isbn: '0-553-21311-3',
+        price: 8.99,
+      },
+      {
+        category: 'fiction',
+        author: 'J. R. R. Tolkien',
+        title: 'The Lord of the Rings',
+        isbn: '0-395-19395-8',
+        price: 22.99,
+      },
+    ],
+    bicycle: {
+      color: 'red',
+      price: 19.95,
+    },
+  },
+  expensive: 10,
+};
+
+export default function App() {
+  const [result, setResult] = React.useState('');
+  const [input, setInput] = React.useState(JSON.stringify(DEFAULT_INPUT, null, 2));
+  const [jsonPath, setJsonPath] = React.useState(`$.store.book[?(@.price>20 || @.category == 'reference')]`);
+
+  const onInputChange = (text: string) => {
+    setInput(text);
+  };
+
+  const onJsonPathChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setJsonPath(event.target.value);
+  };
+
+  React.useEffect(() => {
+    try {
+      const res = query(JSON.parse(input), jsonPath);
+      setResult(JSON.stringify(res, null, 2));
+    } catch (e) {
+      setResult('');
+      console.log(e);
+    }
+  }, [input, jsonPath]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Typography mt={2} variant="h4">
+          JsonPathly Editor
+        </Typography>
+        <Stack my={3}>
+          <TextField label="JSON Path" onChange={onJsonPathChange} value={jsonPath} />
+        </Stack>
+        <Grid container component="main" sx={{ height: '80vh' }}>
+          <Grid item xs={6}>
+            <AceEditor mode="json" theme="monokai" onChange={onInputChange} value={input} />
+          </Grid>
+          <Grid item xs={6}>
+            <AceEditor mode="json" theme="monokai" value={result} />
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
+}
